9d5f68a03bd7ed735d2425fcf8bb52abf9c5ca47
Daniel Jette <daniel.jette@neofinancial.com>
2021-09-30 19:05:37 -0400

    Test of exec

--- main.go
+++ main.go
@@ -1,155 +1,181 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
+    "fmt"
+    "os"
+    "path/filepath"
+    "strings"
+    "os/exec"
 
-	"github.com/bitrise-io/go-steputils/stepconf"
-	"github.com/bitrise-io/go-steputils/tools"
-	"github.com/bitrise-io/go-utils/log"
-	"github.com/bitrise-steplib/bitrise-step-build-router-start/bitrise"
+    "github.com/bitrise-io/go-steputils/stepconf"
+    "github.com/bitrise-io/go-steputils/tools"
+    "github.com/bitrise-io/go-utils/log"
+    "github.com/bitrise-steplib/bitrise-step-build-router-start/bitrise"
 )
 
 const envBuildSlugs = "ROUTER_STARTED_BUILD_SLUGS"
 
 // Config ...
 type Config struct {
-	AppSlug                string          `env:"BITRISE_APP_SLUG,required"`
-	BuildSlug              string          `env:"BITRISE_BUILD_SLUG,required"`
-	BuildNumber            string          `env:"BITRISE_BUILD_NUMBER,required"`
-	AccessToken            stepconf.Secret `env:"access_token,required"`
-	WaitForBuilds          string          `env:"wait_for_builds"`
-	BuildArtifactsSavePath string          `env:"build_artifacts_save_path"`
-	AbortBuildsOnFail      string          `env:"abort_on_fail"`
-	Workflows              string          `env:"workflows,required"`
-	Environments           string          `env:"environment_key_list"`
-	IsVerboseLog           bool            `env:"verbose,required"`
+    AppSlug                string          `env:"BITRISE_APP_SLUG,required"`
+    BuildSlug              string          `env:"BITRISE_BUILD_SLUG,required"`
+    BuildNumber            string          `env:"BITRISE_BUILD_NUMBER,required"`
+    AccessToken            stepconf.Secret `env:"access_token,required"`
+    WaitForBuilds          string          `env:"wait_for_builds"`
+    BuildArtifactsSavePath string          `env:"build_artifacts_save_path"`
+    AbortBuildsOnFail      string          `env:"abort_on_fail"`
+    Workflows              string          `env:"workflows,required"`
+    Environments           string          `env:"environment_key_list"`
+    IsVerboseLog           bool            `env:"verbose,required"`
 }
 
 func failf(s string, a ...interface{}) {
-	log.Errorf(s, a...)
-	os.Exit(1)
+    log.Errorf(s, a...)
+    os.Exit(1)
+}
+
+func ExecuteCommand(executable string, a ...string) {
+    executablePath, _ := exec.LookPath( executable )
+
+    args := append([]string {executablePath}, a...)
+    cmd := &exec.Cmd {
+        Path: executablePath,
+        Args: args,
+        Stdout: os.Stdout,
+        Stderr: os.Stdout,
+    }
+
+    if err := cmd.Run(); err != nil {
+        failf("Error", err)
+    }
 }
 
 func main() {
-	var cfg Config
-	if err := stepconf.Parse(&cfg); err != nil {
-		failf("Issue with an input: %s", err)
-	}
 
-	stepconf.Print(cfg)
-	fmt.Println()
+    ExecuteCommand("go", "version")
+    ExecuteCommand("git", "--version")
+    ExecuteCommand("adb", "--version")
+    ExecuteCommand("gradlew", "--version")
+    log.Infof("Hello, world!")
 
-	log.SetEnableDebugLog(cfg.IsVerboseLog)
+    os.Exit(0)
 
-	app := bitrise.NewAppWithDefaultURL(cfg.AppSlug, string(cfg.AccessToken))
+    var cfg Config
+    if err := stepconf.Parse(&cfg); err != nil {
+        failf("Issue with an input: %s", err)
+    }
 
-	build, err := app.GetBuild(cfg.BuildSlug)
-	if err != nil {
-		failf("failed to get build, error: %s", err)
-	}
+    stepconf.Print(cfg)
+    fmt.Println()
 
-	log.Infof("Starting builds:")
+    log.SetEnableDebugLog(cfg.IsVerboseLog)
 
-	var buildSlugs []string
-	environments := createEnvs(cfg.Environments)
-	for _, wf := range strings.Split(strings.TrimSpace(cfg.Workflows), "\n") {
-		wf = strings.TrimSpace(wf)
-		startedBuild, err := app.StartBuild(wf, build.OriginalBuildParams, cfg.BuildNumber, environments)
-		if err != nil {
-			failf("Failed to start build, error: %s", err)
-		}
-		if startedBuild.BuildSlug == "" {
-			failf("Build was not started. This could mean that manual build approval is enabled for this project and it's blocking this step from starting builds.")
-		}
-		buildSlugs = append(buildSlugs, startedBuild.BuildSlug)
-		log.Printf("- %s started (https://app.bitrise.io/build/%s)", startedBuild.TriggeredWorkflow, startedBuild.BuildSlug)
-	}
+    app := bitrise.NewAppWithDefaultURL(cfg.AppSlug, string(cfg.AccessToken))
 
-	if err := tools.ExportEnvironmentWithEnvman(envBuildSlugs, strings.Join(buildSlugs, "\n")); err != nil {
-		failf("Failed to export environment variable, error: %s", err)
-	}
+    build, err := app.GetBuild(cfg.BuildSlug)
+    if err != nil {
+        failf("failed to get build, error: %s", err)
+    }
 
-	if cfg.WaitForBuilds != "true" {
-		return
-	}
+    log.Infof("Starting builds:")
 
-	fmt.Println()
-	log.Infof("Waiting for builds:")
+    var buildSlugs []string
+    environments := createEnvs(cfg.Environments)
+    for _, wf := range strings.Split(strings.TrimSpace(cfg.Workflows), "\n") {
+        wf = strings.TrimSpace(wf)
+        startedBuild, err := app.StartBuild(wf, build.OriginalBuildParams, cfg.BuildNumber, environments)
+        if err != nil {
+            failf("Failed to start build, error: %s", err)
+        }
+        if startedBuild.BuildSlug == "" {
+            failf("Build was not started. This could mean that manual build approval is enabled for this project and it's blocking this step from starting builds.")
+        }
+        buildSlugs = append(buildSlugs, startedBuild.BuildSlug)
+        log.Printf("- %s started (https://app.bitrise.io/build/%s)", startedBuild.TriggeredWorkflow, startedBuild.BuildSlug)
+    }
 
-	if err := app.WaitForBuilds(buildSlugs, func(build bitrise.Build) {
-		var failReason string
-		switch build.Status {
-		case 0:
-			log.Printf("- %s %s", build.TriggeredWorkflow, build.StatusText)
-		case 1:
-			log.Donef("- %s successful", build.TriggeredWorkflow)
-		case 2:
-			log.Errorf("- %s failed", build.TriggeredWorkflow)
-			failReason = "failed"
-		case 3:
-			log.Warnf("- %s aborted", build.TriggeredWorkflow)
-			failReason = "aborted"
-		case 4:
-			log.Infof("- %s cancelled", build.TriggeredWorkflow)
-			failReason = "cancelled"
-		}
+    if err := tools.ExportEnvironmentWithEnvman(envBuildSlugs, strings.Join(buildSlugs, "\n")); err != nil {
+        failf("Failed to export environment variable, error: %s", err)
+    }
 
-		if cfg.AbortBuildsOnFail == "yes" && build.Status > 1 {
-			for _, buildSlug := range buildSlugs {
-				if buildSlug != build.Slug {
-					abortErr := app.AbortBuild(buildSlug, "Abort on Fail - Build [https://app.bitrise.io/build/"+build.Slug+"] "+failReason+"\nAuto aborted by parent build")
-					if abortErr != nil {
-						log.Warnf("failed to abort build, error: %s", abortErr)
-					}
-					log.Donef("Build " + buildSlug + " aborted due to associated build failure")
-				}
-			}
-		}
+    if cfg.WaitForBuilds != "true" {
+        return
+    }
 
-		if build.Status != 0 {
-			buildArtifactSaveDir := strings.TrimSpace(cfg.BuildArtifactsSavePath)
-			if buildArtifactSaveDir != "" {
-				artifactsResponse, err := build.GetBuildArtifacts(app)
-				if err != nil {
-					log.Warnf("failed to get build artifacts, error: %s", err)
-				}
-				for _, artifactSlug := range artifactsResponse.ArtifactSlugs {
-					artifactObj, err := build.GetBuildArtifact(app, artifactSlug.ArtifactSlug)
-					if err != nil {
-						log.Warnf("failed to get build artifact, error: %s", err)
-					}
-					fullBuildArtifactsSavePath := filepath.Join(buildArtifactSaveDir, artifactObj.Artifact.Title)
-					downloadErr := artifactObj.Artifact.DownloadArtifact(fullBuildArtifactsSavePath)
-					if downloadErr != nil {
-						log.Warnf("failed to download artifact, error: %s", downloadErr)
-					}
-					log.Donef("Downloaded: " + artifactObj.Artifact.Title + " to path " + fullBuildArtifactsSavePath)
-				}
-			}
-		}
-	}); err != nil {
-		failf("An error occoured: %s", err)
-	}
+    fmt.Println()
+    log.Infof("Waiting for builds:")
+
+    if err := app.WaitForBuilds(buildSlugs, func(build bitrise.Build) {
+        var failReason string
+        switch build.Status {
+        case 0:
+            log.Printf("- %s %s", build.TriggeredWorkflow, build.StatusText)
+        case 1:
+            log.Donef("- %s successful", build.TriggeredWorkflow)
+        case 2:
+            log.Errorf("- %s failed", build.TriggeredWorkflow)
+            failReason = "failed"
+        case 3:
+            log.Warnf("- %s aborted", build.TriggeredWorkflow)
+            failReason = "aborted"
+        case 4:
+            log.Infof("- %s cancelled", build.TriggeredWorkflow)
+            failReason = "cancelled"
+        }
+
+        if cfg.AbortBuildsOnFail == "yes" && build.Status > 1 {
+            for _, buildSlug := range buildSlugs {
+                if buildSlug != build.Slug {
+                    abortErr := app.AbortBuild(buildSlug, "Abort on Fail - Build [https://app.bitrise.io/build/"+build.Slug+"] "+failReason+"\nAuto aborted by parent build")
+                    if abortErr != nil {
+                        log.Warnf("failed to abort build, error: %s", abortErr)
+                    }
+                    log.Donef("Build " + buildSlug + " aborted due to associated build failure")
+                }
+            }
+        }
+
+        if build.Status != 0 {
+            buildArtifactSaveDir := strings.TrimSpace(cfg.BuildArtifactsSavePath)
+            if buildArtifactSaveDir != "" {
+                artifactsResponse, err := build.GetBuildArtifacts(app)
+                if err != nil {
+                    log.Warnf("failed to get build artifacts, error: %s", err)
+                }
+                for _, artifactSlug := range artifactsResponse.ArtifactSlugs {
+                    artifactObj, err := build.GetBuildArtifact(app, artifactSlug.ArtifactSlug)
+                    if err != nil {
+                        log.Warnf("failed to get build artifact, error: %s", err)
+                    }
+                    fullBuildArtifactsSavePath := filepath.Join(buildArtifactSaveDir, artifactObj.Artifact.Title)
+                    downloadErr := artifactObj.Artifact.DownloadArtifact(fullBuildArtifactsSavePath)
+                    if downloadErr != nil {
+                        log.Warnf("failed to download artifact, error: %s", downloadErr)
+                    }
+                    log.Donef("Downloaded: " + artifactObj.Artifact.Title + " to path " + fullBuildArtifactsSavePath)
+                }
+            }
+        }
+    }); err != nil {
+        failf("An error occoured: %s", err)
+    }
 }
 
 func createEnvs(environmentKeys string) []bitrise.Environment {
-	environmentKeys = strings.Replace(environmentKeys, "$", "", -1)
-	environmentsKeyList := strings.Split(environmentKeys, "\n")
+    environmentKeys = strings.Replace(environmentKeys, "$", "", -1)
+    environmentsKeyList := strings.Split(environmentKeys, "\n")
 
-	var environments []bitrise.Environment
-	for _, key := range environmentsKeyList {
-		if key == "" {
-			continue
-		}
+    var environments []bitrise.Environment
+    for _, key := range environmentsKeyList {
+        if key == "" {
+            continue
+        }
 
-		env := bitrise.Environment{
-			MappedTo: key,
-			Value:    os.Getenv(key),
-		}
-		environments = append(environments, env)
-	}
-	return environments
+        env := bitrise.Environment{
+            MappedTo: key,
+            Value:    os.Getenv(key),
+        }
+        environments = append(environments, env)
+    }
+    return environments
 }
