7651662c97f2aacda6dbca9f7c9913c1b8b1c98f
Daniel Jette <daniel.jette@neofinancial.com>
2021-10-02 18:56:15 -0400

    doesn't seem to be working :shrug:

--- env/env.go
+++ env/env.go
@@ -22,9 +22,9 @@ func SetTargetEnv() {
         util.Failf("Issue with an input: %s", err)
     }
 
-    adbCommand := fmt.Sprintf("adb shell am instrument -w -m -e debug false %s/%s", cfg.TestPackage, cfg.TestRunner)
+    adbCommand := fmt.Sprintf("\"adb shell am instrument -w -m -e debug false %s/%s\"", cfg.TestPackage, cfg.TestRunner)
     log.Infof("Set adb command to [%s]", adbCommand)
     execmd.ExecuteCommand("envman", "add", "--key", "ADB_COMMAND", "--value", adbCommand)
     log.Infof("Set target apk to [%s]", cfg.APK)
-    execmd.ExecuteCommand("envman", "add", "--key", "TARGET_APK", "--value", cfg.APK)
+    execmd.ExecuteCommand("envman", "add", "--key", "TARGET_APK", "--value", fmt.Sprintf("\"%s\"",cfg.APK))
 }
